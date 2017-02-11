5afcad50e99777dc7d131c8b2e935d496c67fc92
leestick <leestick@mail.ru>
2017-02-11 15:59:40 +0300

    1 я домашка начало

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Main2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Main2.java
@@ -5,12 +5,11 @@ public class Main2 {
     hello("world");
     hello("user");
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.area() + " = " + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b));
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
     }
 
   public static void hello(String somebody) {
@@ -18,13 +17,7 @@ public class Main2 {
 
   }
 
-  public static double area(double l) {
-    return l * l;
-  }
 
-  public static double area(double a, double b) {
-    return a * b;
-  }
 
 
 
---
+++ sandbox/src/main/java/ru/stqa/work/lenght/Point.java
@@ -0,0 +1,10 @@
+package ru.stqa.work.lenght;
+
+/**
+ * Created by ioK on 11.02.2017.
+ */
+public class Point {
+  public double x;
+  public double y;
+
+}
