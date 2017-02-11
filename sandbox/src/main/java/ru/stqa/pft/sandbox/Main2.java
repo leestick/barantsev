0f1e69bcbdf3aea7a146a75f949fd0a5d31c2bb0
leestick <leestick@mail.ru>
2017-02-11 12:42:57 +0300

    Добавлены методы area

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Main2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Main2.java
@@ -1,8 +1,31 @@
 package ru.stqa.pft.sandbox;
 
-public class Main {
+public class Main2 {
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b));
     }
+
+  public static void hello(String somebody) {
+     System.out.println("Hello, " + somebody);
+
+  }
+
+  public static double area(double l) {
+    return l * l;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+
+
+
 }
